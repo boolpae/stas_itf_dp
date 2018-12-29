3a661a33200f06506e5639573adc5547f390a75d
이승훈-집 <boolpae@gmail.com>
2018-12-29 22:20:25 +0900

    신규 모듈(RedisHandler) 파일 미추가로 인한 re-commit

---
+++ include/RedisHandler.h
@@ -0,0 +1,29 @@
+#ifndef _REDIS_HANDLER_H_
+#define _REDIS_HANDLER_H_
+
+#include "xRedisClient.h"
+
+
+using namespace xrc;
+
+class RedisHandler
+{
+public:
+    virtual ~RedisHandler();
+
+    static RedisHandler* instance();
+	xRedisClient& getRedisClient() { return m_xRedis; }
+
+private:
+    RedisHandler();
+    static void thrdKeepAlive(RedisHandler *handler);
+
+private:
+    static RedisHandler* m_instance;
+
+	xRedisClient m_xRedis;
+};
+
+
+
+#endif // _REDIS_HANDLER_H_
